29100ef40b98af0a8d353e2b0af36299edc350e3
densinasunny <66365796+densinasunny@users.noreply.github.com>
2020-06-06 13:07:09 +0530

    Add files via upload

---
+++ code employee.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+ 
+/*structure declaration*/
+struct employee{
+    char    name[30];
+    int     empage;
+    int     empphone;
+    float   salary;
+};
+ 
+int main()
+{
+    
+    
+    printf("name     age   phone number    salary   \n");        
+
+     
+
+    
+   
+    
+ 
+   printf("chirag    24    1234567788      20000\n");
+   printf("arnav     31    1234567891      56000\n");
+   printf("shivam     45   8881101111      30500");
+    
+    
+    
+    return 0; 
+}
+
